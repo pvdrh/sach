a4ea40079cee6a5499db84ac723ffce774057ec7
Phạm Văn Duy <637915@sv.vnua.edu.vn>
2022-11-28 00:07:10 +0700

    Fix login form

--- resources/views/backend/auth/login.blade.php
+++ resources/views/backend/auth/login.blade.php
@@ -30,12 +30,14 @@
 <body>
 
 <div class="limiter">
-    <div class="container-login100" style="background: #f8694a;">
+    <div class="container-login100" style="background: rgb(131,58,180);
+background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);">
         <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
             <form class="login100-form validate-form" method="POST" action="{{ route('login.store') }}">
                 @csrf
 					<span class="login100-form-title p-b-49 ">
-						Đăng nhập
+{{--						Đăng nhập--}}
+                        <h1>Book<span style="color: #F8694A">cloud</span></h1>
 					</span>
 
                 <div class="wrap-input100 validate-input m-b-23" data-validate = "Email không được bỏ trống">
@@ -56,15 +58,15 @@
                     <span class="focus-input100" data-symbol="&#xf190;"></span>
                 </div>
 
-                <div class="text-right p-t-8 p-b-31">
-                    <a href="#">
-                        Quên mật khẩu?
-                    </a>
-                </div>
+{{--                <div class="text-right p-t-8 p-b-31">--}}
+{{--                    <a href="#">--}}
+{{--                        Quên mật khẩu?--}}
+{{--                    </a>--}}
+{{--                </div>--}}
                 @if(session()->has('fail'))
                     <div class="alert alert-danger">{!! session('fail') !!}</div>
                 @endisset
-                <div class="container-login100-form-btn">
+                <div style="margin-top: 50px" class="container-login100-form-btn">
                     <div class="wrap-login100-form-btn">
                         <div class="login100-form-bgbtn"></div>
                         <button class="login100-form-btn">
@@ -73,32 +75,32 @@
                     </div>
                 </div>
 
-                <div class="txt1 text-center p-t-54 p-b-20">
-						<span>
-							Đăng nhập bằng
-						</span>
-                </div>
+{{--                <div class="txt1 text-center p-t-54 p-b-20">--}}
+{{--						<span>--}}
+{{--							Đăng nhập bằng--}}
+{{--						</span>--}}
+{{--                </div>--}}
 
-                <div class="flex-c-m">
-                    <a href="#" class="login100-social-item bg1">
-                        <i class="fa fa-facebook"></i>
-                    </a>
+{{--                <div class="flex-c-m">--}}
+{{--                    <a href="#" class="login100-social-item bg1">--}}
+{{--                        <i class="fa fa-facebook"></i>--}}
+{{--                    </a>--}}
 
-                    <a href="#" class="login100-social-item bg2">
-                        <i class="fa fa-twitter"></i>
-                    </a>
+{{--                    <a href="#" class="login100-social-item bg2">--}}
+{{--                        <i class="fa fa-twitter"></i>--}}
+{{--                    </a>--}}
 
-                    <a href="#" class="login100-social-item bg3">
-                        <i class="fa fa-google"></i>
-                    </a>
-                </div>
+{{--                    <a href="#" class="login100-social-item bg3">--}}
+{{--                        <i class="fa fa-google"></i>--}}
+{{--                    </a>--}}
+{{--                </div>--}}
 
-                <div class="flex-col-c p-t-155">
+{{--                <div class="flex-col-c p-t-155">--}}
 
-                    <a href="{{ route('register.form') }}" class="txt2">
-                        Đăng kí tài khoản
-                    </a>
-                </div>
+{{--                    <a href="{{ route('register.form') }}" class="txt2">--}}
+{{--                        Đăng kí tài khoản--}}
+{{--                    </a>--}}
+{{--                </div>--}}
             </form>
         </div>
     </div>
