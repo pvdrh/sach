c199a30d9b896c1a9cca369ebb8d241ca44ca98b
duypv <duyphamvan@bizfly.vn>
2022-12-08 13:57:04 +0700

    1

--- resources/views/backend/orders/success.blade.php
+++ resources/views/backend/orders/success.blade.php
@@ -8,9 +8,14 @@
             </div><!-- /.col -->
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a style="text-decoration: none" href="{{ route('backend.dashboard') }}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
-                                <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
-                            </svg> Trang chủ</a></li>
+                    <li class="breadcrumb-item"><a style="text-decoration: none"
+                                                   href="{{ route('backend.dashboard') }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 class="bi bi-house" viewBox="0 0 16 16">
+                                <path
+                                    d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
+                            </svg>
+                            Trang chủ</a></li>
                     <li class="breadcrumb-item active">Đơn hàng đã giao</li>
                 </ol>
             </div><!-- /.col -->
@@ -49,14 +54,13 @@
                                     <td>{{$order->products_count}}</td>
                                     <td>{{$order->shipping}}</td>
                                     <td style="text-align: center">
-                                        <form action="{{ route('backend.order.destroy', $order->id) }}"
-                                              method="POST">
-                                            {{ csrf_field() }}
-                                            {{ method_field('DELETE') }}
-                                            <a href="#" class="btn btn-info">Chi tiết</a>
-                                            <button class="btn btn-danger"><i class="fa fa-btn fa-trash"></i> Xóa
-                                            </button>
-                                        </form>
+                                        <a href="{{route('order.show', $order->id)}}" class="btn btn-primary">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                 fill="currentColor" class="bi bi-info" viewBox="0 0 16 16">
+                                                <path
+                                                    d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                                            </svg>
+                                            Chi tiết</a>
                                     </td>
                                 </tr>
                             @endforeach
